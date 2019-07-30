bc36d0d6bb0406e99587ed68767ddb1803053c98
Gigi M <mmgamao1@gmail.com>
2019-07-30 16:48:07 +0000

    Automatically backed up by Learn

--- db/migrate/005_add_day_genre_season_to_show.rb
+++ db/migrate/005_add_day_genre_season_to_show.rb
@@ -1,5 +1,5 @@
  
-class AddDayAndSeasonToShow < ActiveRecord::Migration[5.1]
+class AddDayGenreSeasonToShow < ActiveRecord::Migration[5.1]
   def change
     add_column :shows, :day, :string
     add_column :shows, :season, :string
